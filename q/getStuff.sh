6d9b6401d1ee48c51d00e6cff2feab418eeed1fd
Emil <emilioo@gmail.com>
2016-07-10 14:08:59 +0000

    minor changey

--- q/getStuff.sh
+++ q/getStuff.sh
@@ -3,6 +3,6 @@
 . ~/.bashrc
 export LD_LIBRARY_PATH=$HOME/omrepo/q/zlib-1.2.8:$LD_LIBRARY_PATH
 export QHOME=$HOME/omrepo/q
-cd $HOME/q
+cd $HOME/omrepo/q
 #./l32/q -p 8765 -u pwd
 nohup ./l32/q ../getOptionsData.q -p 8765 -u pwd  > $HOME/ompreo/q/getOptionsData.log 2>&1 
