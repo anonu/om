2ecaec507f886183ddb8487ecaebcf0565f01020
anonu <emilioo@gmail.com>
2016-08-03 22:37:25 +0000

    minor change

--- q/getStuff.sh
+++ q/getStuff.sh
@@ -5,4 +5,4 @@ export LD_LIBRARY_PATH=$HOME/omrepo/q/zlib-1.2.8:$LD_LIBRARY_PATH
 export QHOME=$HOME/omrepo/q
 cd $HOME/omrepo/q
 #./l32/q -p 8765 -u pwd
-nohup ./l32/q ../getOptionsData.q -p 8765 -u pwd  > $HOME/ompreo/q/getOptionsData.log 2>&1 
+nohup ./l32/q ../getOptionsData.q -p 8766 -u pwd  > $HOME/omrepo/q/getOptionsData.log 2>&1 
